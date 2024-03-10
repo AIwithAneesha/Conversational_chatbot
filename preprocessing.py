d8eb14ed87e9f0e259d1555bca3b22a759b55fce
AIwithAneesha <aneeshabsoman1994@gmail.com>
2024-03-10 21:49:47 +0530

    first commit with files

---
+++ preprocessing.py
@@ -0,0 +1,125 @@
+
+import dotenv, requests, uuid, time, re, os
+from datetime import datetime, timedelta
+import pytz, dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+
+dotenv.load_dotenv()
+openai = ChatOpenAI(model_name='gpt-3.5-turbo')
+WEATHER_API = os.environ.get('WEATHER_API')
+CITY = os.environ.get('CITY')
+
+class PreProcessingRemainder:
+
+    def __init__(self):
+        pass
+
+    def find_output_frok_fewshot_template(self, examples):
+        example_template = '\n User:{sentence}\nAI: {title},/n/n/n/n,{datetime}\n'
+        example_prompt = PromptTemplate(input_variables=['query', 'answer'],template=example_template)
+        prefix = 'The following are excerpts from conversations with an AI assistant. The assistant is known for cutting the sentence into title and datetime.Make sure to remove the timezone always .Here are some\n        examples:\n        '
+        suffix = '\n User: {sentence}\nAI:'
+        few_shot_prompt_template = FewShotPromptTemplate(examples=examples,
+          example_prompt=example_prompt,
+          prefix=prefix,
+          suffix=suffix,
+          input_variables=['sentence'],example_separator=',/n/n/n/n,')
+        chain = LLMChain(llm=openai, prompt=few_shot_prompt_template)
+        return chain
+
+    def find_title_datetime(self, text):
+        examples = [
+         {'sentence':'Set a reminder for a meeting at 2 AM tommorow', 
+          'title':'Meeting reminder',  'datetime':'2023-09-12T02:00:00'},
+         {'sentence':'Remind me of a 3 PM conference call', 
+          'title':'Conference call reminder',  'datetime':'2023-09-11T15:00:00'},
+         {'sentence':'Create an reminder for lunch at 12:30 PM', 
+          'title':'Lunch reminder',  'datetime':'2023-09-11T12:30:00'},
+         {'sentence':"Schedule a doctor's appointment for tomorrow at 10:30 AM", 
+          'title':'Doctors appointment reminder',  'datetime':'2023-09-12T10:30:00'},
+         {'sentence':'Create a reminder for a project review at 3:00 PM on Friday', 
+          'title':'Project review reminder',  'datetime':'2023-09-15T10:30:00'},
+         {'sentence':'Set up a meeting with John for next Monday at 9 AM', 
+          'title':'Meeting with John reminder',  'datetime':'2023-09-15T10:30:00'},
+         {'sentence':'Remind me to call the client at 4:45 PM on Wednesday', 
+          'title':'Meeting with client reminder',  'datetime':'2023-09-13T14:45:00'},
+         {'sentence':'Plan a conference call with the team for 2:30 PM on the 20th of this month', 
+          'title':'Conference call reminder',  'datetime':'2023-09-20T14:30:00'}]
+        # text = 'Schedule a meeting at 4:20pm on 10th March'
+        chain = self.find_output_from_fewshot_template(examples)
+        result = chain.invoke(text)
+        print(result)
+        title_date_time = result['text'].split(',/n/n/n/n,')
+        title = title_date_time[0]
+        date_time = title_date_time[1]
+        date_time = datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
+        time_zone = pytz.timezone('Asia/Kolkata')
+        date_time = time_zone.localize(date_time)
+        end_time = date_time + timedelta(minutes=30)
+        date_time = date_time.strftime('%Y-%m-%dT%H:%M:%S%Z')
+        end_time = end_time.strftime('%Y-%m-%dT%H:%M:%S%Z')
+        return (
+         title, date_time, end_time)
+
+
+class PreProcessingWeather:
+
+    def __init__(self):
+        pass
+
+    def fetch_weather():
+        url = f"http://api.weatherapi.com/v1/current.json?key={WEATHER_API}&q={CITY}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            res = response.json()
+            temp_c = res['current']['temp_c']
+            temp_f = res['current']['temp_f']
+            condition = res['current']['condition']['text']
+            return f"The temperature in Celsius and Fahrenheit of {CITY} are {temp_c} and {temp_f}. It is {condition} currently in {CITY}."
+        print('Error:', response.status_code)
+
+
+class FinalOutput:   #to customize each of the output as per the input
+
+    def __init__(self):
+        pass
+
+    def remove_last_user(text):
+        words = text.split()
+        if words and words[-1] == "User":
+            words.pop()
+        result = ' '.join(words)
+
+        return result
+
+    def final_output(self, text, category):
+        if category in ('therapist', 'depressed', 'hurt', 'stress', 'addiction', 'anxious','therapy', 'mental'):
+            output =text
+            print(text)
+            return output
+        
+        elif category in ('doctor'):
+            print(text)
+            output =text
+            return output
+        
+        elif category in ('weather'):
+            print(text)
+            output =text
+            return output
+        
+        elif category in ['general knowledge question','Tell a joke']:
+            print(text)
+            output = text
+            return output
+
+        else:
+            print('None')
+            output = None
+            return output
---
+++ server.py
@@ -0,0 +1,274 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+import streamlit as st
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import create_tagging_chain,create_tagging_chain_pydantic
+
+from langchain_openai import ChatOpenAI
+import datetime
+import os.path
+import uuid
+import time
+import re
+from datetime import datetime, timedelta
+import os 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from langchain_community.llms import HuggingFaceEndpoint
+from langchain_community.llms import HuggingFaceHub
+
+
+import openai
+from langchain.prompts import (
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
+
+from langchain.chains import LLMChain
+
+import dotenv
+from server.Conversational_chatbot.preprocessing import PreProcessingRemainder,PreProcessingWeather,FinalOutput
+
+
+remainder=PreProcessingRemainder()
+weather=PreProcessingWeather()
+finaloutput=FinalOutput()
+
+
+dotenv.load_dotenv()
+
+HUGGINGFACEHUB_API_TOKEN = os.environ.get('HUGGINGFACEHUB_API_TOKEN')
+
+llm=ChatOpenAI(temperature=0,model='gpt-3.5-turbo-0613')
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+#########################################################################################
+
+
+def create_unique_request_id():
+        unique_id = str(uuid.uuid4())
+        timestamp_ms = int(time.time() * 1000)
+        request_id = f'{unique_id}-{timestamp_ms}'
+        return request_id
+
+def remainder_pushing_to_google_calendar(title,text,start_time,end_time):
+
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            flow.redirect_uri = 'http://localhost:8080' 
+            creds = flow.run_local_server(port=8080)  
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+  
+    calendar_id="primary"
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+       
+        event = {
+            'summary': title,
+            'description': text,
+            'start': {
+                'dateTime': start_time,
+                'timeZone': 'Asia/Kolkata', 
+            },
+            'end': {
+                'dateTime': end_time,
+                'timeZone': 'Asia/Kolkata',
+            },
+            'reminders': {'useDefault': False,'overrides': [{'method': 'popup', 'minutes': 30}]}}
+        calendar_id = 'primary'
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        print(f"Event created:{event.get('htmlLink')}")
+        #database.push_reminder_to_database(request_id,title,text,start_time,end_time,action_taken)
+        return event
+    
+    except HttpError as error:
+        print(error)
+
+
+
+from langchain.chains import LLMChain
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage)
+
+
+def question_to_a_therapist_only_once(text,mood): #no storage in vector database
+
+    llm = HuggingFaceHub(repo_id='tiiuae/falcon-7b-instruct', huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN)
+    question ="As a therapist respond empathetically and effectively to this statment: "+text+"I am right now feeling "+mood+'Please dont give explanation of the output you give'
+    template = """Question: {question}
+    Answer: Let's go through this togethere."""
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])    
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    
+    result=llm_chain.invoke(question)
+    print(result)
+    output=result['text']
+    return output
+
+
+def question_to_a_therapist_multiple_chat(text,mood): #not done-store in vector database and memory
+    question = +text+"I am right now feeling "+mood
+
+    messages = [
+        SystemMessage(content="You are an excellent therapist who responds empathetically and effectively.Please dont give explanation of the output you give"),
+        HumanMessage(content=question)
+    ]
+
+    return messages
+
+def question_to_a_doctor_only_once(text,mood):
+    llm = HuggingFaceHub(repo_id='tiiuae/falcon-7b-instruct', huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN)
+    question ="As a Doctor respond with medical therapies and home remedies to this statment: "+text+"I am right now feeling "+mood+'Please dont give explanation of the output you give'
+    template = """Question: {question}
+    Answer: Let's go through this togethere."""
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])    
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    
+    result=llm_chain.invoke(question)
+    print(result)
+    output=result['text']
+    return output
+
+
+
+
+def generalknowledge_only_once(text,mood):
+    llm = HuggingFaceHub(repo_id='tiiuae/falcon-7b-instruct', huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN)
+    question ="As a knowledge hub give answer to the question: "+text
+    template = """Question: {question}
+    Answer: Let's go through this togethere."""
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])    
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    
+    result=llm_chain.invoke(question)
+    print(result)
+    output=result['text']
+    output=finaloutput.remove_last_user(output)
+    return output
+
+
+############classify################################################################
+
+def classify_using_sentence_embedding(): #use sentence emdedding and nearest neighbor
+    pass
+
+
+def classify_using_lanchain(input):
+    schema = {
+    "properties": {
+        "mood": {
+            "type": "string",
+            "enum": ['happy','sad','angry','anxious','scared','hurt'],
+            "description": "describes what is the mood as per the linguistic pattern",
+        },
+        "language": {
+            "type": "string",
+            "enum": ["spanish", "english", "french", "german", "italian"],
+        },
+        "Task": {
+            "type": "string",
+            "enum": [ "doctor","mental","therapy" ,"anxious","depressed","hurt","stress",'addiction', "weather related","Remainder","Play a video","Play a game","Play a music","Tell a joke", "general knowledge question"],
+            "description": "describes what is the task asked to do in the text",
+        },
+
+    },
+    "required": ["mood", "language", "Task"],}
+    chain = create_tagging_chain(schema, llm)
+    result=chain.invoke(input)
+    print(result)
+    return result['input'],result['text']['mood'],result['text']['language'],result['text']['Task']
+
+
+####################send the the api ##########################################
+
+def send_to_respective_API(text,category,mood):
+
+    if category=='Remainder':
+        title,date_time,end_time=remainder.find_title_datetime(text)
+        if end_time is not None:
+            remainder_pushing_to_google_calendar(title,text,date_time,end_time)
+            return 'Success'
+        else:
+            return None
+        
+    
+    elif category=='question to a doctor':
+        print("category",category)
+        output=question_to_a_doctor_only_once(text,mood)
+
+
+    elif category in ['therapist', 'depressed', 'hurt', 'stress', 'addiction', 'anxious','therapy','mental']:
+        print("category",category)
+        output=question_to_a_therapist_only_once(text,mood)
+        if output is not None:
+            return output
+
+
+    elif category=='weather related':
+        output=weather.fetch_weather()
+        return output
+
+
+    elif category=='Play a video': #yet to build
+        print("category",category)
+        return category
+    
+
+    elif category=='Play a game': #yet to build
+        print("category",category)
+        return category
+
+    elif category=='Play a music':
+        print("category",category)
+        return category
+    
+    elif category in ['general knowledge question','Tell a joke']:
+        output=generalknowledge_only_once(text,mood)
+        print(output)
+        return output
+
+#######################Streamlit home page##############################
+if 'result_of_task' not in  st.session_state:
+        st.session_state.result_of_task=''
+
+
+def main():
+    st.title("Aneesha's Chatbot App")
+    st.text("Welcome to Aneesha's Chatbot. I want to help you with your questions, so ask away!")
+    text = st.text_area("Enter your text here:")
+    
+    if st.button("Submit"):
+        _,mood,_,category=classify_using_lanchain(text)
+        st.session_state.result_of_task=send_to_respective_API(text,category,mood)
+        if st.session_state.result_of_task!='':
+            result = finaloutput.final_output(st.session_state.result_of_task,category)
+            st.write(result)
+        else:
+            st.warning("Please enter some text.")
+
+if __name__ == "__main__":
+    main()
+
+#streamlit run server.py
